b994dbbb554059ddc52af82e6b207f7eee4bc7eb
srconde <srconde@esei.uvigo.es>
2018-05-16 15:37:45 +0200

    ya se filtra por libreria en el map

--- Practica10/PR10GR1/EjemplaresVendidosNovelasLibrerias.py
+++ Practica10/PR10GR1/EjemplaresVendidosNovelasLibrerias.py
@@ -1,3 +1,10 @@
+def split_fileBook(line):
+    (book,store) = line.split(',')
+    if store == libreria:
+        return (book, store)
+    else:
+        return ("", "")
+
 def main(sc, libreria):
     # Load the books lookup dictionary
     books = sc.textFile("join2_book*.txt").map(split_fileBook)
@@ -8,7 +15,7 @@ def main(sc, libreria):
     nums = sc.textFile("join2_num*.txt").map(split_fileNum)
     nums_joined_books = nums.join(books).collect()
 
-    nums_joined_books = nums_joined_books.reduceByKey(sum_counts).collect()
+    #nums_joined_books = nums_joined_books.reduceByKey(sum_counts).collect()
 
     # Map the total delay to the airline (joined using the broadcast value)
     #counts = nums.map(lambda b: (book_lookup.value[b.book], b.count))
@@ -17,8 +24,8 @@ def main(sc, libreria):
     #counts = sorted(counts, key=itemgetter(1))
 
     for entry in nums_joined_books:
-        if entry[1][1] == libreria:
-            print "%s: %s" % (entry[0], entry[1][1])
+        #if entry[1][1] == libreria:
+        print "%s: %s" % (entry[0], entry[1][1])
             # Show a bar chart of the delays
             #plot(delays)
 
@@ -30,7 +37,7 @@ sc = SparkContext(appName="novelasLibrerias")
 sc.setLogLevel("WARN")
 from code import *
 import sys
-libreria=sys.argv[1]
+libreria = sys.argv[1]
 print ("Obteniendo ventas de novelas ofrecidas por "+libreria)
 
 # Execute Main functionality
--- Practica10/PR10GR1/code.py
+++ Practica10/PR10GR1/code.py
@@ -3,9 +3,7 @@ def split_fileNum(line):
     count = int(count)
     return (book,count)
 
-def split_fileBook(line):
-    (book,store) = line.split(',')
-    return (book, store)
+
 
 def sum_counts(a, b):
     return a + b
